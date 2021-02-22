2de3178e3b5d489060f9f0aa4b5ad49b4e81305b
Colby Biegler <xyzaire@gmail.com>
2021-02-22 03:39:03 -0800

    Update scoreboard

--- Assets/Code/Score.cs
+++ Assets/Code/Score.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public Text scoreText;
+    private string moneyString;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey("Score"))
+            PlayerPrefs.SetInt("Score", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerPrefs.HasKey("Score")){
+            moneyString = "Money: " + PlayerPrefs.GetInt("Score");
+            scoreText.text = moneyString;
+            //Debug.Log("Changed " + moneyString, scoreText);
+        }
     }
 }
